b2e437dbae5c0afb9530e394674b298caed15d10
JosafatJimenezB <josa221001@gmail.com>
2022-03-17 18:27:17 -0600

    add: añadiendo el nombre, id e imagen del pokemon

---
+++ script.js
@@ -0,0 +1,47 @@
+const fetchPokemon = () => {
+    
+    const pokeNameInput = document.getElementById("pokeName");
+    let pokeName = pokeNameInput.value;
+    pokeName = pokeName.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+
+    fetch(url).then((res) => {
+        if (res.status != "200") {
+            console.log(res);
+        }
+        else {
+            return res.json();
+        }
+
+    }).then((data) => {
+        if (data) {
+            console.log(data);
+            let pokeNombre = data.name;
+            nombrePokemon(pokeNombre);
+
+            let pokeNumero = data.id;
+            idPokemon(pokeNumero);
+
+            let pokeImg = data.sprites.front_default;
+            pokeImage(pokeImg);
+        }
+    });
+}
+
+const pokeImage = (url) => {
+
+    const pokePhoto = document.getElementById("pokeImg");
+    pokePhoto.src = url;
+}
+
+
+const nombrePokemon = (url) => {
+    const pokeNombr = document.getElementById('pokeNombre');
+    pokeNombr.innerHTML = url;
+}
+
+const idPokemon = (url) => {
+    const pokeNum = document.getElementById('pokeId');
+    pokeNum.innerHTML = url;
+}
+
